67ce9952c87e51ebe09e1f6595843ae0fa1fffff
燎 石橋 <150461@jc-21.jp>
2017-12-06 16:02:35 +0900

    115

--- SQLite11/SQLite11/SelectPage.xaml.cs
+++ SQLite11/SQLite11/SelectPage.xaml.cs
@@ -9,15 +9,16 @@ using Xamarin.Forms.Xaml;
 
 namespace SQLite11
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SelectPage : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectPage : ContentPage
+    {
         public SelectPage()
         {
             InitializeComponent();
-        }
-        void SelectClicked(object sender, EventArgs e)
-        {
+
+            //selectイベントハンドラ
+
+
             //Userテーブルの行データを取得
             var query = UserModel.selectUser(); //中身はSELECT * FROM [User]
             var layout = new StackLayout { HorizontalOptions = LayoutOptions.Center, Margin = new Thickness { Top = 100 } };
@@ -26,12 +27,9 @@ namespace SQLite11
                 //Userテーブルの名前列をLabelに書き出す
                 layout.Children.Add(new Label { Text = user.Name });
             }
-            //selectする
-           
-            layout.Children.Add(Select);
-            Select.Clicked += SelectClicked;
-            Content = layout;
 
+
+            Content = layout;
         }
     }
 }
